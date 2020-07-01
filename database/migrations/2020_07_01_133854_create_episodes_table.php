054f0680d52132eeb2ccd85e9f407bb74052927c
qusaix <qusaiznemat6@gmail.com>
2020-07-01 16:42:45 +0300

    getting started

---
+++ database/migrations/2020_07_01_133854_create_episodes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('duration');
+            $table->string('time');
+            $table->string('thumbnail');
+            $table->longText('video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
